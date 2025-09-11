9299e124d0dc566e1dfc682a5fa4e4348d04aed8
ReinerLau <lk850593913@gmail.com>
2025-09-11 11:28:26 +0800

    Add new HTML and JavaScript files for layout testing: created 'forced-reflow.html' and 'render-timing.html' to demonstrate dynamic height changes, and added 'init.js' for bulk div creation.

---
+++ layout/init.js
@@ -0,0 +1,7 @@
+for (let index = 0; index < 1000; index++) {
+  const div = document.createElement("div");
+  div.style.height = "100px";
+  div.style.marginTop = "10px";
+  div.style.backgroundColor = "red";
+  document.body.appendChild(div);
+}
